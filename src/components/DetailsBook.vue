ba2d450a9fa4faaca452d905fb0de8277759e63d
Santiago Lopez <santi@atrevise.com>
2025-04-08 06:13:54 +0200

    Ajout onbeforemount sur listofbooks

---
+++ src/components/DetailsBook.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+console.log(route.params.id)
+</script>
+
+<template>
+
+</template>
--- src/components/ListOfBooksView.vue
+++ src/components/ListOfBooksView.vue
@@ -1,29 +1,45 @@
 <script setup>
-import { ref } from "vue";
+import { onBeforeMount, ref } from "vue";
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
 const apiURL = "http://localhost:3000/books";
 const refBooks = ref([]);
 const fetchItems = async () => {
   try {
     const response = await fetch(apiURL);
     refBooks.value = await response.json();
-    console.log(refBooks.value);
+    // console.log(refBooks.value);
   } catch (error) {
     console.log(error);
   }
 };
 
-fetchItems();
+const goToDetailsHandler = (bookId)=>{
+  console.log('go to details?', bookId)
+  router.push({
+    name:'details',
+    params:{
+      bookId
+    }
+  })
+}
+
+onBeforeMount(async() => {
+  await fetchItems();
+})
 </script>
 
 <template>
   
-  <table class="text-center table mx-auto ">
+  <table class="text-center table mx-auto w-12/12 ">
     <thead>
       <tr>
         <th>id</th>
         <th>title</th>
         <th>author</th>
         <th>year</th>
+        <th>Details</th>
       </tr>
     </thead>
       <tbody>
@@ -31,8 +47,19 @@ fetchItems();
           <td>{{ book._id }}</td>
           <td>{{ book.title }}</td>
           <td>{{ book.author.firstName }} {{ book.author.lastName }}</td>
-          <td>{{ book.publicationYear }}</td> 
+          <td>{{ book.publicationYear }}</td>
+          <td @click="goToDetailsHandler(book._id)">Details</td>
         </tr>
       </tbody>
   </table>
 </template>
+
+<style scoped>
+td {
+  padding: 10px;
+  border: 1px solid black;
+  background: #0000003a;
+}
+
+
+</style>
