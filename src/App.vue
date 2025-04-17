4352ac314bd4d2a0f4293ee8d29d3c5e90066ca2
Santiago <santi@atrevise.com>
2025-04-17 19:04:39 +0200

    maj

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <script setup>
-// docker compose build
-// docker compose up -d
-// Ces commandes en allant dans le dossier du projet et en lançant la commande.
-// https://github.com/Thinkedou/Nest-Ts-Api
-
-// import { ref } from "vue";
 import { RouterView } from "vue-router";
 import SlideBar from "./components/SidebarView.vue";
-// const apiURL = "http://localhost:3000/books";
-// const refBooks = ref([]);
-// const fetchItems = async () => {
-//   try {
-//     const response = await fetch(apiURL);
-//     refBooks.value = await response.json();
-//     console.log(refBooks.value);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
 
-// fetchItems();
 </script>
 
 <template>
